351663d155183e515e3edc18dafddb974824976a
mandard8 <mandard8@gmail.com>
2019-10-08 17:14:55 +0530

    Javascript COde

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+
+function add()
+        {
+            var a = 5;
+            var b = 2;
+            var c;
+            c=a+b;
+            document.write("Addition is:" + c +"<br>");
+        }
+    
+        function sub()
+        {
+            var a=30;
+            var b=20;
+            var c;
+            c=a-b;
+            document.write("subtraction  is: " + c +"<br>");
+        }
+        function mul()
+        {
+            var a=5;
+            var b=6;
+            var c;
+            c=a*b;
+            document.write("multiplication is: " + c +"<br>");
+        }
+        function div()
+        {
+            var a=5;
+            var b=2;
+            var c;
+            c=a/b;
+            document.write("division is: " + c +"<br>");
+        }
+
+        addn();
+        sub();
+        mul();
+        div();
