a2c53c9d4041ccfb16e9249851f4e1e89ca98fd3
mandard8 <mandard8@gmail.com>
2019-10-09 14:26:20 +0530

    Javascript Code

---
+++ arithmetic.js
@@ -0,0 +1,31 @@
+var a=10;
+var b=5;
+
+
+function addn()
+{
+    var c = a+b;
+    console.log(c);
+}
+addn();
+
+function sub()
+{
+    var c = a-b;
+    console.log(c);
+}
+sub();
+
+function mul()
+{
+    var c = a*b;
+    console.log(c);
+}
+mul();
+
+function div()
+{
+    var c = a/b;
+    console.log(c);
+}
+div();
