32ab2b4b66c35b1755e490eaaa604db73af689b8
mandard8 <mandard8@gmail.com>
2019-10-13 16:15:54 +0530

    reversestring program

---
+++ reversestring.js
@@ -0,0 +1,8 @@
+function string_reverse(str) 
+{
+    return str.split("").reverse().join("");
+}
+
+console.log(string_reverse("mandar"));
+console.log(string_reverse("codekul"));
+console.log(string_reverse("kothrud"));
